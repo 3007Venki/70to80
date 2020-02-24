a9bff7aa6b8567c2f672aa0dafa5a2b60d11c986
3007Venki <61415188+3007Venki@users.noreply.github.com>
2020-02-25 00:41:15 +0530

    Add files via upload

---
+++ 70to80.py
@@ -0,0 +1,18 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> for x in range(ord("F"),ord("Q")):
+	print (x)
+
+	
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+>>> 
